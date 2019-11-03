87c177db0bf57415f78bd449f45b4bb63d3021ed
Eric <ericmuchiri02@gmail.com>
2019-11-03 20:24:27 +0300

    numerics and operations

---
+++ 02basics/numbers.py
@@ -0,0 +1,22 @@
+#integers - Whole numbers
+#Floats - Numbers with decimals
+#Getting the type that data belongs to
+print(type(9)) 
+print(type(0.9))
+#When you do math involving both an int and a float, the result is a float
+print(9 + 0.9)
+print(9 + 9.0)
+#addition
+print(23+73)
+#subtraction
+print(40-21.4)
+# Division
+# Returns a float even if no float is involved in the equation
+print(1/2)
+#**- Exponential (raise to power)
+print(2**3)
+print(49**0.5)
+# %- Modulus remainder
+print(14%3)
+# // - integer division - Gives an int instead of a float
+print(10//3)
