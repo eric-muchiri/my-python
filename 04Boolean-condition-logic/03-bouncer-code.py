7be00a1b1d6466446c1ac48ce6d138d64f146ac5
Eric <ericmuchiri02@gmail.com>
2019-11-22 23:33:53 +0300

    multiconditions

--- 04Boolean-condition-logic/03-bouncer-code.py
+++ 04Boolean-condition-logic/03-bouncer-code.py
@@ -1,11 +1,17 @@
 #ask for age
 age = input("How old are you: ")
-#18-21 wristband
-if int(age) >=18 and int(age) < 21:
-    print("You need a wristband")
-#21+ can drink
-elif int(age) >= 21:
-    print("you can drink")
-#too young
+#cast into an int
+age = int(age)
+#check if input is empty
+if age:
+    #18-21 wristband
+    if age >=18 and age < 21:
+        print("You need a wristband")
+    #21+ can drink
+    elif age >= 21:
+        print("you can drink")
+    #too young
+    else:
+        print("sorry you are too young")
 else:
-    print("sorry you are too young")
+    print("Please enter an age")
