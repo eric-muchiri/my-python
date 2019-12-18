3aaa56c730a5a8be0f87ce07986bdb6a477b874a
Eric <ericmuchiri02@gmail.com>
2019-12-18 11:33:35 +0300

    conditions on numbers

--- 05Looping/test.py
+++ 05Looping/test.py
@@ -4,3 +4,16 @@ num = int(num)
 
 for time in range (num):
     print("Hello")
+#loop2 unlucky
+#tesnNum = input("Input your test number: ")
+#tesnNum = int(tesnNum)
+for number in range (21):
+    if number == 4 or number == 13:
+        print(number)
+        print(" unlucky")
+    if number%2 == 0 and number != 4:
+        print(number)
+        print(" even")
+    if number%2 == 1 and number != 13:
+        print(number)
+        print(" odd")        
