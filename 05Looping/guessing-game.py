32516c39944dbb8d2f1c7224d3ce0d8a911e7ea8
Eric <ericmuchiri02@gmail.com>
2020-01-22 23:15:31 +0300

    conditionals: using if statements

--- 05Looping/guessing-game.py
+++ 05Looping/guessing-game.py
@@ -1,4 +1,12 @@
 import random
 #get a eandom number between one and 10 inclusive
 random_number = random.randint(1, 10)
-print(random_number)
+#print(random_number)
+guess = input("pick a number between 1 and 10: ")
+guess = int(guess)
+if guess == random_number:
+    print("you got it")
+elif guess > random_number:
+    print("Too high")
+else:
+    print("Too low")
