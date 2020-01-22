86adc78bee05df51da221194e54f1ee923096001
Eric <ericmuchiri02@gmail.com>
2020-01-22 23:51:33 +0300

    using while

--- 05Looping/guessing-game.py
+++ 05Looping/guessing-game.py
@@ -4,8 +4,10 @@ random_number = random.randint(1, 10)
 #print(random_number)
 guess = input("pick a number between 1 and 10: ")
 guess = int(guess)
-if guess == random_number:
-    print("you got it")
+# if guess == random_number:
+    # print("you got it")
+while guess != random_number:
+    
 elif guess > random_number:
     print("Too high")
 else:
