80749b413902aa50d9d885e8448b469d220a9e1f
Eric <ericmuchiri02@gmail.com>
2020-01-27 22:27:01 +0300

    a simple guessing game

--- 05Looping/guessing-game.py
+++ 05Looping/guessing-game.py
@@ -2,13 +2,17 @@ import random
 #get a eandom number between one and 10 inclusive
 random_number = random.randint(1, 10)
 #print(random_number)
-guess = input("pick a number between 1 and 10: ")
-guess = int(guess)
+guess = None
+#guess = input("pick a number between 1 and 10: ")
+#guess = int(guess)
 # if guess == random_number:
     # print("you got it")
 while guess != random_number:
-    
-elif guess > random_number:
-    print("Too high")
-else:
-    print("Too low")
+    guess = input("pick a number between 1 and 10: ")
+    guess = int(guess)
+    if guess < random_number:
+        print("Too low")
+    elif guess > random_number:
+        print("Too high")
+    else:
+        print("You won")
