9d19b895d1fc37d9bc79fb4eb6db4de715f6d748
Eric <ericmuchiri02@gmail.com>
2020-11-30 22:42:44 +0300

    comprehension vs looping

--- 06lists/list-comprehension.py
+++ 06lists/list-comprehension.py
@@ -1,3 +1,14 @@
 #list comprehension
 nums = [1, 2, 3]
 print([x * 10 for x in nums])
+
+#looping
+
+numbers = [1, 2, 3]
+doubled_numbers = []
+
+for num in numbers:
+    doubled_number = num * 2
+    doubled_numbers.append(doubled_number)
+
+print(doubled_numbers);
