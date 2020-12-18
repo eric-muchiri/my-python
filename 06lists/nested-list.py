0c062fb3231a5ffd44f22f920a8c2cd89f1b9a70
Eric <ericmuchiri02@gmail.com>
2020-12-18 22:39:53 +0300

    looping lists

--- 06lists/nested-list.py
+++ 06lists/nested-list.py
@@ -1,4 +1,9 @@
 nested_list = [[1,2,3], [4,5,6], [7,8,9]]
 
-print(nested_list)
-print(nested_list[0][1])
+#print(nested_list)
+#print(nested_list[0][1])
+
+#printning values in nested lists
+for i in nested_list:
+    for val in i:
+        print(val)
