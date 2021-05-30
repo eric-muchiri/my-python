e69ef9c538b971de081a7038fb5fcc9e9555c761
Eric <ericmuchiri02@gmail.com>
2021-05-30 23:50:48 +0300

    tuple methods

--- 08Tuples-sets/tuples.py
+++ 08Tuples-sets/tuples.py
@@ -5,4 +5,15 @@ print(months)
 for month in months:
     print(month)
 
+# tupe methods
+#count-ount number of appearances
+x = (1, 2, 3, 4, 5, 5, 5)
 
+print(x.count(1))
+print (x.count(5))
+
+#index-gives the index of certain value
+#counts from 0
+
+print(x.index(3))
+print(x.index(5))
