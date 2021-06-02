a14c996bba9d084de3380925bdd6de49608d5778
Eric <ericmuchiri02@gmail.com>
2021-06-02 23:29:15 +0300

    set methods

--- 08Tuples-sets/sets.py
+++ 08Tuples-sets/sets.py
@@ -1,3 +1,19 @@
 #do not have duplicate values
 #elements are not oredered
 s={1,3,4,5}
+
+#accessing
+print(4 in s)# returns either true or false
+
+for number in s:
+    print(number)
+
+#set methods
+#add
+s.add(6)
+
+print(s)
+
+#remove
+s.remove(4)
+print(s)
