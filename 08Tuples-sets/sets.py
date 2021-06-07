e39ec76c9b8a36a459c27a88ad2cafc41ecba334
Eric <ericmuchiri02@gmail.com>
2021-06-07 23:29:58 +0300

    set methods

--- 08Tuples-sets/sets.py
+++ 08Tuples-sets/sets.py
@@ -17,3 +17,20 @@ print(s)
 #remove
 s.remove(4)
 print(s)
+
+#set math
+#union | combine sets with no duplicates
+
+s1={1,2,3}
+s2={2,3,4,5}
+
+print(s1 | s2)
+
+#intersection &
+#print elements that appear in both sets
+
+print(s1 & s2)
+
+#comprehension
+sq={x**2 for x in range(10)}
+print(sq)
