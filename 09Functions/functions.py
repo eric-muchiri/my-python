3eb58d715629e7715ddcc816cc078942522157f4
Eric <ericmuchiri02@gmail.com>
2021-06-09 22:58:51 +0300

    intro to functions

---
+++ 09Functions/functions.py
@@ -0,0 +1,7 @@
+def sing_happy_birthday():
+    print('happy birthday to you')
+    print('happy birthday to you')
+    print('happy birthday dear you')
+    print('happy birthday to you')
+
+sing_happy_birthday()
