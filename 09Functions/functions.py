fd16b4c0804ada660caca96e626627048e76a535
Eric <ericmuchiri02@gmail.com>
2021-08-04 22:58:41 +0300

    happy birthday with parameters

--- 09Functions/functions.py
+++ 09Functions/functions.py
@@ -5,3 +5,12 @@ def sing_happy_birthday():
     print('happy birthday to you')
 
 sing_happy_birthday()
+
+#birthday with parameters
+def sing_happy_birthday2(name):
+    print('happy birthday to you')
+    print('happy birthday to you')
+    print(f'happy birthday dear {name}')
+    print('happy birthday to you')
+
+sing_happy_birthday2("eric")
