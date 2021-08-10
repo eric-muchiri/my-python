b69e0509377ff59db6000f12ef45166f4c525cce
Eric <ericmuchiri02@gmail.com>
2021-08-10 22:56:40 +0300

    passing default parameters

---
+++ 09Functions/exponent.py
@@ -0,0 +1,11 @@
+def exponent(num, power):
+	return num ** power
+
+print(exponent(2,3)) #8
+
+
+def exponent2(num, power=2):
+	return num ** power
+
+print(exponent2(3)) #9
+print(exponent2(7)) #49
